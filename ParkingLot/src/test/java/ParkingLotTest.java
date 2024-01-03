65fe7e0db4917663bbc0128c744a9fd43203de6e
akshi0409 <akshita.asah@gmail.com>
2024-01-03 22:48:36 +0530

    Added UC 5 - Notify to remove sign

--- ParkingLot/src/test/java/ParkingLotTest.java
+++ ParkingLot/src/test/java/ParkingLotTest.java
@@ -17,11 +17,23 @@ public class ParkingLotTest {
         Assert.assertEquals(true,parkingLot.removeCar("123"));
     }
     @Test
-    /* UC 3, find if car is full */
+    /* UC 3, find if car is full, UC 4 - To alert full */
     public void givenCapacity_CheckLotFull_ReturnFullOrNot()
     {
         ParkingLot parkingLot = new ParkingLot();
         Assert.assertTrue(parkingLot.findCapacity());
     }
-    
+    @Test
+    /* UC 5 - notify to remove full sign*/
+    public void givenCapacity_CheckFull_NotifyToRemoveFullSign()
+    {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.parkCar("2");
+        parkingLot.parkCar("3");
+        parkingLot.parkCar("4");
+        parkingLot.parkCar("5");
+        Assert.assertFalse(parkingLot.notifyFull());
+        parkingLot.removeCar("2");
+        Assert.assertTrue(parkingLot.notifyFull());
+    }
 }
